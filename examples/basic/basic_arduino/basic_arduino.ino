90381b3511c859fb4d5a6b2dd2b976ef0d2ec91a
gohai <gottfried.haider@gmail.com>
2021-09-14 00:15:41 +0800

    Add basic example

---
+++ examples/basic/basic_arduino/basic_arduino.ino
@@ -0,0 +1,25 @@
+int lines = 0;
+unsigned long lastSent = 0;
+
+void setup() {
+  Serial.begin(57600);
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    if (input.length() > 0) {
+      // this would be the place to parse, 
+      // and act upon the message from the
+      // computer
+      lines++;
+    }
+  }
+
+  unsigned long now = millis();
+  if (now-lastSent > 3000) {
+    Serial.print(lines);
+    Serial.println(" messages received");
+    lastSent = now;
+  }
+}
---
+++ examples/basic/basic_p5js/sketch.js
@@ -0,0 +1,46 @@
+let port;
+let connectBtn;
+let sendBtn;
+
+let textmovers = [];
+
+function setup() {
+  createCanvas(600, 400);
+
+  connectBtn = createButton('Connect to Arduino');
+  connectBtn.position(10, 10);
+  connectBtn.mousePressed(connect);
+
+  sendBtn = createButton('Send');
+  sendBtn.position(10, 10);
+  sendBtn.hide();
+  sendBtn.mousePressed(send);
+}
+
+function draw() {
+  background(0);
+
+  if (port) {  // the port might not have been opened here
+    let input = port.readUntil('\n');
+    if (input.length > 0) {
+      textmovers.push(new TextMover(input.trim(), true));
+    }
+  }
+
+  for (let i=0; i < textmovers.length; i++) {
+    textmovers[i].move();
+    textmovers[i].display();
+  }
+}
+
+function connect() {
+  port = new WebSerial('Arduino', 57600);
+  connectBtn.hide();
+  sendBtn.show();
+}
+
+function send() {
+  let output = 'Hello from the computer';
+  port.write(output);
+  textmovers.push(new TextMover(output, false));
+}
